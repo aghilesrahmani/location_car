d95b3676ba2906dee47db31c8ad83cc146b37921
aghiles <ariles.rahmani@gmail.com>
2021-02-12 22:09:52 +0100

    ajout du boutton LogIn

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,27 +1,52 @@
+import { HeaderBackground } from '@react-navigation/stack';
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
+import { Button, StyleSheet,Image, ImageBackground, Alert } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import {StatusBar} from 'expo-status-bar';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
+    
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <ImageBackground source={require('/Users/antoine/AwesomeProject/ferrari.jpg')}
+      style={styles.img}
+      ></ImageBackground>
+      
+      <Text style={styles.title}> Location de Voiture</Text>
+      
+      <TextInput style={styles.inputBox} placeholder='Email' placeholderTextColor='white'></TextInput>
+      
+      <TextInput style={styles.inputBox} placeholder='Password' placeholderTextColor='white'></TextInput>
+    
+    <View style={styles.Btn}>
+      <Button title="Login"     color='white' onPress={() => Alert.alert('bonjour')}/>
+      </View>
     </View>
-  );
+
+  ); 
+
 }
+HeaderBackground
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'white',
+  },
+  img:{
+    width:'100%',
+    height:'100%',
+    resizeMode:'cover',
+    position:'absolute',
+    justifyContent:'center'
   },
   title: {
-    fontSize: 20,
+    bottom: 200,
+    fontSize: 40,
     fontWeight: 'bold',
   },
   separator: {
@@ -29,4 +54,27 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  inputBox: {
+    bottom:-180,
+    fontSize:25,
+    width:300,
+    height:60,
+    borderRadius:29,
+    color:'black',
+    backgroundColor: 'black',
+    textAlign:'center',
+    marginVertical:10
+    
+    
+    //flex:1,
+    
+  },
+
+  Btn:{
+    bottom:-190,
+    borderRadius:10,
+    backgroundColor: 'black',
+    
+
+  }
 });
---
+++ screens/login.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class login extends Component {
+    render(){
+        return(
+            <View style={StyleSheet.cotainer}>
+
+                <Text style={Style.txt}>welcome to my App</Text>
+                <Button></Button>
+
+
+            </View>
+        )
+    }
+
+}
